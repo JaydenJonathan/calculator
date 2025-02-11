46ae292443ced02b7efdf981ed98f9533beb5681
Jayden Jonathan <jayden@192.168.0.251>
2025-02-11 19:39:09 +0700

    Initial Code

---
+++ hi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int na = 0;
+  int nb = 0;
+  scanf("%d", &na);
+  scanf("%d", &nb);
+  int results = na + nb;
+  printf("result: %d", results);
+  return 0;
+}
